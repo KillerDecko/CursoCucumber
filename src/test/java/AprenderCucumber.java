79e3ab0476d1d38abfe5f2edd75895ea300b2e4c
DBServer - André Mendonça <andre.dbserver@e-unicred.com.br>
2018-08-15 12:11:31 -0300

    Initial commit

---
+++ src/test/java/AprenderCucumber.java
@@ -0,0 +1,26 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+public class AprenderCucumber {
+	@Given("^que criei o arquivos corretamente$")
+	public void queCrieiOArquivosCorretamente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^executa-lo$")
+	public void executaLo() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^a especificacao deve finalizar com sucesso$")
+	public void aEspecificacaoDeveFinalizarComSucesso() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
---
+++ src/test/java/aprender_cucumber.feature
@@ -0,0 +1,11 @@
+Feature: Aprender Cucumber
+  Como um aluno
+  Eu quero aprnder a utilizar Cucumber
+  Para que eu possa automatizar criterio de aceitacao
+
+Scenario: Deve executar especificacao
+    Given que criei o arquivos corretamente
+    When executa-lo
+    Then a especificacao deve finalizar com sucesso
+
+
